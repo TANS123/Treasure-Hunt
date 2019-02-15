047973de81866673262361fdc85968fea1d387bd
TANS123 <44278831+TANS123@users.noreply.github.com>
2019-02-14 16:33:13 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,56 @@
+$("h1").click(function() {
+    $("#pic").show();
+});
+
+$("h1").click(function() {
+    $("#puppies").show();
+});
+
+$("#pic").hover(function() {
+    $("#secpic").show();
+});
+
+$("#pic").hover(function() {
+    $("#mouse").show();
+});
+
+$("#mouse").mouseenter(function() {
+    $("#piccc").show();
+});
+
+$("#mouse").mouseenter(function() {
+    $("#button").show();
+});
+
+$("#button").dblclick(function() {
+    $("#clown").show();
+});
+
+$("#button").dblclick(function() {
+    $("#button").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#piccc").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#pic").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#secpic").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#mouse").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#text").hide();
+});
+
+$("#button").dblclick(function() {
+    $("#puppies").hide();
+});
+
